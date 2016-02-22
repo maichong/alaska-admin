d21c53eeb75dcdc60b5708a24c444b73bace3db8
Liang Xingchen <liang@maichong.it>
2016-02-22 16:29:09 +0800

    main script

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @copyright Maichong Software Ltd. 2016 http://maichong.it
- * @date 2016-01-28
- * @author Liang <liang@maichong.it>
- */
-
-'use strict';
-
-module.export = require('./lib/alaska-admin');
