6b5e4450aea0f385d590306e534c4edc9b17687f
Liang Xingchen <liang@maichong.it>
2016-02-29 21:11:43 +0800

    register roles

--- src/alaska-admin.js
+++ src/alaska-admin.js
@@ -13,4 +13,27 @@ export default class AdminService extends alaska.Service {
     options.dir = __dirname;
     super(options, alaska);
   }
+
+  async postLoadModels() {
+    let userService = this.service('user');
+
+    let abilities = [{
+      name: 'admin',
+      desc: '登录管理平台'
+    }];
+    let roles = [{
+      name: 'admin',
+      desc: '管理员',
+      abilities: ['admin']
+    }];
+
+    for (let ability of abilities) {
+      ability.service = this.id;
+      await userService.registerAbility(ability);
+    }
+    for (let role of roles) {
+      role.service = this.id;
+      await userService.registerRole(role);
+    }
+  }
 }
